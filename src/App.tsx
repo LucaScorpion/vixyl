1f2c774ccd02483cdd7bdaf963ac16e5794f6f6b
Luca Scalzotto <luca@scorpiac.com>
2020-06-02 19:42:22 +0200

    Working image reading, inputs for start coords

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const App: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [imageData, setImageData] = useState('');
+  const [startX, setStartX] = useState<number>(0);
+  const [startY, setStartY] = useState<number>(0);
+
+  useEffect((): void => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+
+    if (!canvas || !context) {
+      return;
+    }
+
+    // Load the image onto the canvas.
+    const image = new Image();
+    image.addEventListener('load', () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.drawImage(image, 0, 0);
+    });
+    image.src = imageData;
+  }, [imageData]);
 
   return (
     <div>
@@ -9,30 +30,41 @@ const App: React.FC = () => {
         Vixyl
       </header>
       <main>
-        <input type='file' onChange={(e) => {
-          const file = e.currentTarget.files?.item(0);
-          if (!file) {
-            return;
-          }
-
-          const reader = new FileReader();
-          reader.addEventListener('load', () => {
-            const canvas = canvasRef.current;
-            const context = canvas?.getContext('2d');
-            if (typeof reader.result !== 'string' || !canvas || !context) {
+        <input
+          type='file'
+          onChange={(e) => {
+            // Check if a file is selected.
+            const file = e.currentTarget.files?.item(0);
+            if (!file) {
               return;
             }
 
-            const image = new Image();
-            image.src = reader.result;
-            image.addEventListener('load', () => {
-              context.drawImage(image, 0, 0);
-
+            // Read the file into state.
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+              if (typeof reader.result === 'string') {
+                setImageData(reader.result);
+              }
             });
-          });
-          reader.readAsDataURL(file);
+            reader.readAsDataURL(file);
+          }} />
+        <canvas ref={canvasRef} style={{
+          display: 'block',
+          marginTop: 24,
+          marginBottom: 24,
         }} />
-        <canvas ref={canvasRef} />
+        <input
+          type='number'
+          min={0}
+          onChange={(e) => setStartX(parseInt(e.currentTarget.value, 10))}
+          value={startX}
+        />
+        <input
+          type='number'
+          min={0}
+          onChange={(e) => setStartY(parseInt(e.currentTarget.value, 10))}
+          value={startY}
+        />
       </main>
     </div>
   );
