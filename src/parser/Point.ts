5437efebdf5819693ce6b391b4ce088136a7c837
Luca Scalzotto <luca@scorpiac.com>
2020-06-02 20:53:06 +0200

    Working vinyl reading

--- src/parser/Point.ts
+++ src/parser/Point.ts
@@ -2,3 +2,7 @@ export interface Point {
   x: number;
   y: number;
 }
+
+export function isSamePoint(a: Point, b: Point): boolean {
+  return a.x === b.x && a.y === b.y;
+}
--- src/parser/VinylParser.ts
+++ src/parser/VinylParser.ts
@@ -1,5 +1,5 @@
 import { PixelData } from './PixelData';
-import { Point } from './Point';
+import { isSamePoint, Point } from './Point';
 
 export default class VinylParser {
   constructor(private readonly context: CanvasRenderingContext2D) {
@@ -17,9 +17,12 @@ export default class VinylParser {
       data.push(pixel.red);
 
       // Get the next pixel position.
+      const nextPos = this.findNextPixel(prevPos, pos);
       prevPos = pos;
-      pos = this.findNextPixel(prevPos, pos);
+      pos = nextPos;
     }
+
+    console.log(data);
 
     return data;
   }
@@ -34,7 +37,40 @@ export default class VinylParser {
     });
   }
 
+  private isInBounds(pos: Point): boolean {
+    return pos.x >= 0 && pos.y >= 0 && pos.x < this.context.canvas.width && pos.y < this.context.canvas.height;
+  }
+
   private findNextPixel(previous: Point, current: Point): Point | null {
+    for (let dX = -1; dX <= 1; dX++) {
+      for (let dY = -1; dY <= 1; dY++) {
+        const nextPoint = ({
+          x: current.x + dX,
+          y: current.y + dY,
+        });
+
+        // Check if the point is the same as the previous or current point.
+        if (isSamePoint(nextPoint, previous) || isSamePoint(nextPoint, current)) {
+          continue;
+        }
+
+        // Check if the point is in bounds.
+        if (!this.isInBounds(nextPoint)) {
+          continue;
+        }
+
+        // Get the pixel data, check if it is valid.
+        const data = this.getPixelData(nextPoint);
+        if (this.isDataPixel(data)) {
+          return nextPoint;
+        }
+      }
+    }
+
     return null;
+  }
+
+  private isDataPixel(data: PixelData): boolean {
+    return (data.red !== 0 || data.green !== 0 || data.blue !== 0) && data.alpha !== 0;
   }
 }
