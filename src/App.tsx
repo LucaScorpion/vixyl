affa800f721a6eca5fd000d0d40efbdcea5cc6b4
Luca Scalzotto <luca@scorpiac.com>
2020-06-02 19:26:56 +0200

    Reading image into canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const App: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   return (
     <div>
       <header>
         Vixyl
       </header>
+      <main>
+        <input type='file' onChange={(e) => {
+          const file = e.currentTarget.files?.item(0);
+          if (!file) {
+            return;
+          }
+
+          const reader = new FileReader();
+          reader.addEventListener('load', () => {
+            const canvas = canvasRef.current;
+            const context = canvas?.getContext('2d');
+            if (typeof reader.result !== 'string' || !canvas || !context) {
+              return;
+            }
+
+            const image = new Image();
+            image.src = reader.result;
+            image.addEventListener('load', () => {
+              context.drawImage(image, 0, 0);
+
+            });
+          });
+          reader.readAsDataURL(file);
+        }} />
+        <canvas ref={canvasRef} />
+      </main>
     </div>
   );
 };
