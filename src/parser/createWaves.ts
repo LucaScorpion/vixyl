6f72b6e83acae06426fffdcf0ab9709a1b5af351
Luca Scalzotto <luca@scorpiac.com>
2020-06-03 18:42:41 +0200

    Add wav layout info to createWaves functions

--- src/parser/createWaves.ts
+++ src/parser/createWaves.ts
@@ -24,31 +24,72 @@ function getWaveData(data: Uint8Array, format: WaveFormat): Uint8Array {
   return waveData;
 }
 
+/**
+ * Create the "RIFF" chunk.
+ *
+ * Chunk layout:
+ * | Field     | Size | Value |
+ * |-----------|------|-------|
+ * | ChunkID   | 4    | "RIFF" (hex: 52, 49, 46, 46)
+ * | ChunkSize | 4    | 36 + data size (or 4 + 8 + Subchunk1Size + 8 + Subchunk2Size)
+ * | Format    | 4    | "WAVE" (hex: 57, 41, 56, 45)
+ *
+ * @param data The sound data used in the "data" subchunk.
+ */
 function createRiffChunk(data: Uint8Array): Uint8Array {
   const chunk = new Uint8Array(12);
   encodeString(chunk, 0, 'RIFF');
-  encodeInt(chunk, 4, 36 + data.length); // Total chunk size, i.e. number of bytes following this number
+  encodeInt(chunk, 4, 36 + data.length);
   encodeString(chunk, 8, 'WAVE');
   return chunk;
 }
 
+/**
+ * Create the "fmt " subchunk.
+ *
+ * Chunk layout:
+ * | Field         | Size | Value |
+ * |---------------|------|-------|
+ * | ChunkID       | 4    | "fmt " (hex: 66, 6D, 74, 20)
+ * | ChunkSize     | 4    | 16 (for PCM)
+ * | AudioFormat   | 2    | 1 (PCM, i.e. linear quantization)
+ * | Channels      | 2    | 1 = mono, 2 = stereo, etc.
+ * | SampleRate    | 4    |
+ * | ByteRate      | 4    | SampleRate * Channels * (BitsPerSample / 8)
+ * | BlockAlign    | 2    | Channels * (BitsPerSample / 8)
+ * | BitsPerSample | 2    |
+ *
+ * @param format The sound format information.
+ */
 function createFmtChunk(format: WaveFormat): Uint8Array {
   const chunk = new Uint8Array(24);
   encodeString(chunk, 0, 'fmt ');
-  encodeInt(chunk, 4, 16); // Subchunk size = 16 for PCM
-  encodeShort(chunk, 8, 1); // Audio format = 1 for PCM (linear quantization, no compression)
-  encodeShort(chunk, 10, 1); // Audio channels = 1 (mono)
-  encodeInt(chunk, 12, format.sampleRate); // Sample rate
-  encodeInt(chunk, 16, format.sampleRate); // Bitrate = sample rate * channels * bytes per sample
-  encodeShort(chunk, 20, 1); // Block align = channels * bytes per sample
-  encodeShort(chunk, 22, 8); // Bits per sample
+  encodeInt(chunk, 4, 16);
+  encodeShort(chunk, 8, 1); // AudioFormat
+  encodeShort(chunk, 10, 1); // Channels
+  encodeInt(chunk, 12, format.sampleRate); // SampleRate
+  encodeInt(chunk, 16, format.sampleRate); // ByteRate
+  encodeShort(chunk, 20, 1); // BlockAlign
+  encodeShort(chunk, 22, 8); // BitsPerSample
   return chunk;
 }
 
+/**
+ * Create the "data" subchunk.
+ *
+ *  Chunk layout:
+ * | Field     | Size | Value |
+ * |-----------|------|-------|
+ * | ChunkID   | 4    | "data" (hex: 64, 61, 74, 61)
+ * | ChunkSize | 4    | Length of Data, i.e.: Samples * Channels * (BitsPerSample / 8)
+ * | Data      | ?    | Sound data
+ *
+ * @param data The sound data.
+ */
 function createDataChunk(data: Uint8Array): Uint8Array {
   const chunk = new Uint8Array(8 + data.length);
   encodeString(chunk, 0, 'data');
-  encodeInt(chunk, 4, data.length); // Subchunk size = bytes in data = samples * channels * bytes per sample
+  encodeInt(chunk, 4, data.length);
   chunk.set(data, 8);
   return chunk;
 }
