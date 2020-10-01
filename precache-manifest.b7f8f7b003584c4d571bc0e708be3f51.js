92c721fbb56bee56fae5b05547a6204e28724f7b
LucaScorpion <luca@scorpiac.com>
2020-10-01 19:14:22 +0000

    Deploying to gh-pages from  @ 57fa558816d13ac1c30f85e389bf11abd6531582 🚀

---
+++ precache-manifest.b7f8f7b003584c4d571bc0e708be3f51.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfe96c3fbb6f3d83eadcdd84e94ed05",
+    "url": "/vixyl/index.html"
+  },
+  {
+    "revision": "511c2e93d819a8aeee74",
+    "url": "/vixyl/static/css/2.9c5210c4.chunk.css"
+  },
+  {
+    "revision": "3a606be1c29a2a24aa6e",
+    "url": "/vixyl/static/css/main.83a7f1f5.chunk.css"
+  },
+  {
+    "revision": "511c2e93d819a8aeee74",
+    "url": "/vixyl/static/js/2.14af9577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vixyl/static/js/2.14af9577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a606be1c29a2a24aa6e",
+    "url": "/vixyl/static/js/main.47155e22.chunk.js"
+  },
+  {
+    "revision": "6613f563d2232a968d63",
+    "url": "/vixyl/static/js/runtime-main.239cd415.js"
+  },
+  {
+    "revision": "003231a1f009db089ab16093ce59e4d2",
+    "url": "/vixyl/static/media/fa-brands-400.003231a1.eot"
+  },
+  {
+    "revision": "2ec1097c081aa61e17956be4e8008cba",
+    "url": "/vixyl/static/media/fa-brands-400.2ec1097c.ttf"
+  },
+  {
+    "revision": "3237e6d6e151b80ed2686fedbbb280f4",
+    "url": "/vixyl/static/media/fa-brands-400.3237e6d6.svg"
+  },
+  {
+    "revision": "5795bc0a3b45e06502187ca104557ca3",
+    "url": "/vixyl/static/media/fa-brands-400.5795bc0a.woff2"
+  },
+  {
+    "revision": "90cd422fde1f01392a34aaec011a5dda",
+    "url": "/vixyl/static/media/fa-brands-400.90cd422f.woff"
+  },
+  {
+    "revision": "2788556a1a246f0d0167ce5b4189151e",
+    "url": "/vixyl/static/media/fa-regular-400.2788556a.svg"
+  },
+  {
+    "revision": "27a4e6fdc8464141b7a957dba093826a",
+    "url": "/vixyl/static/media/fa-regular-400.27a4e6fd.woff2"
+  },
+  {
+    "revision": "3cefd3390001dcb1eb4629f61bba9185",
+    "url": "/vixyl/static/media/fa-regular-400.3cefd339.ttf"
+  },
+  {
+    "revision": "8a8d8cd6f4c8a8e556d75eb45b317f29",
+    "url": "/vixyl/static/media/fa-regular-400.8a8d8cd6.woff"
+  },
+  {
+    "revision": "ee87e7fcb588d700111c24d5ada28775",
+    "url": "/vixyl/static/media/fa-regular-400.ee87e7fc.eot"
+  },
+  {
+    "revision": "06d5f03672635f0a67010721e8e9929d",
+    "url": "/vixyl/static/media/fa-solid-900.06d5f036.eot"
+  },
+  {
+    "revision": "2eaaf2a78a47f2d9ae8d1d836eed5d24",
+    "url": "/vixyl/static/media/fa-solid-900.2eaaf2a7.ttf"
+  },
+  {
+    "revision": "55071ae1abc18726e23174192ebc0b68",
+    "url": "/vixyl/static/media/fa-solid-900.55071ae1.woff2"
+  },
+  {
+    "revision": "56df75b6af75497c7a9a2b0847981f40",
+    "url": "/vixyl/static/media/fa-solid-900.56df75b6.woff"
+  },
+  {
+    "revision": "8e39d5d50ead4840070075889f5daeaa",
+    "url": "/vixyl/static/media/fa-solid-900.8e39d5d5.svg"
+  },
+  {
+    "revision": "6b381db3ae95f65aa520eb81a8d6504f",
+    "url": "/vixyl/static/media/tdfw_new.6b381db3.png"
+  },
+  {
+    "revision": "a7ac15663cbd73cb9081b3f68c4e26a6",
+    "url": "/vixyl/static/media/tweet.a7ac1566.png"
+  }
+]);
